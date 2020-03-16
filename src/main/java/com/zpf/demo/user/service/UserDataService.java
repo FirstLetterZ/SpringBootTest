32e7bdda2edd4c696ba58e3ec0232b0f5829ad0f
ZPF <pengfei.zhou@andall.com>
2020-03-16 11:06:23 +0800

    进度保存

--- src/main/java/com/zpf/demo/user/service/UserDataService.java
+++ src/main/java/com/zpf/demo/user/service/UserDataService.java
@@ -5,6 +5,7 @@ import com.zpf.demo.user.been.UserPersonalInfo;
 import com.zpf.demo.user.been.UserTimeInfo;
 import com.zpf.demo.user.repository.UserRepository;
 import com.zpf.demo.user.repository.UserTimeRepository;
+import com.zpf.demo.user.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,9 @@ import java.util.List;
 @Service
 @Transactional
 public class UserDataService {
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Autowired
     private UserRepository userRepository;
@@ -28,16 +32,28 @@ public class UserDataService {
         timeInfo.setCreateTime(new Date());
         timeInfo.setActiveTime(new Date());
         userTimeRepository.save(timeInfo);
+        redisUtil.set("lastUser", baseInfo);
     }
 
     public UserPersonalInfo findLastUser() {
-        List<UserPersonalInfo> users = userRepository.findAll();
-        if (users.size() > 0) {
-            return users.get(users.size() - 1);
-        } else {
-            UserPersonalInfo info = new UserPersonalInfo();
-            info.setRealName("test_A");
+        UserPersonalInfo info = null;
+        try {
+            info = (UserPersonalInfo) redisUtil.get("lastUser");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (info != null) {
+            info.setEmail("12345678");
             return info;
         }
+        List<UserPersonalInfo> users = userRepository.findAll();
+        if (users.size() > 0) {
+            info = users.get(users.size() - 1);
+        } else {
+            info = new UserPersonalInfo();
+            info.setRealName("test_A");
+        }
+        redisUtil.set("lastUser", info);
+        return info;
     }
 }
